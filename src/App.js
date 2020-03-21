361ecd2e4c1144f1c41c7561333db012cd9316b7
s3665959 <s3665959@gmail.com>
2020-03-21 17:18:38 +0700

    collection item done

--- src/App.js
+++ src/App.js
@@ -1,29 +1,15 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
-const HatsPage = () => (
-<div>
-  <h1>HAT PAGES NAKA</h1>
-</div>
-);
-
-const JacketsPage = () => (
-  <div>
-    <h1>Jackets Page NAKA</h1>
-  </div>
-  );
-
-
+import ShopPage from './pages/shop/shop.component';
 
 function App() {
   return ( 
   <div> 
    <Switch>
       <Route exact path='/' component={HomePage} />
-      <Route exact path='/hats' component={HatsPage} />
-      <Route exact path='/shop/jackets' component={JacketsPage} />
+      <Route exact path='/shop' component={ShopPage} />
    </Switch>
   </div>
   );
